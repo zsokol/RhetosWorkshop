b21bd26dff5da8dfe0d9c484c9ffb4462e1baff7
zsokol-omega <zsokol@omega-software.hr>
2025-04-02 14:53:59 +0200

    Day1 development environment and the first application  changes made by following the demostrate directive

---
+++ Bookstore/src/Bookstore.Service/Controllers/DemoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rhetos.Processing.DefaultCommands;
+using Rhetos.Processing;
+using Rhetos;
+
+namespace Bookstore.Service.Controllers
+{
+    [Route("demo/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly IProcessingEngine processingEngine;
+        private readonly IUnitOfWork unitOfWork;
+
+        public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork)
+        {
+            this.processingEngine = processingEngine.Value;
+            this.unitOfWork = unitOfWork.Value;
+        }
+
+        [HttpGet]
+        public string ReadBooks()
+        {
+            var readCommandInfo = new ReadCommandInfo { DataSource = "Bookstore.Book", ReadTotalCount = true };
+            var result = processingEngine.Execute(readCommandInfo);
+            return $"{result.TotalCount} books.";
+        }
+
+        [HttpGet]
+        public string WriteBook()
+        {
+            var newBook = new Bookstore.Book { Title = "NewBook" };
+            var saveCommandInfo = new SaveEntityCommandInfo { Entity = "Bookstore.Book", DataToInsert = new[] { newBook } };
+            processingEngine.Execute(saveCommandInfo);
+            unitOfWork.CommitAndClose(); // Commits and closes database transaction.
+            return "1 book inserted.";
+        }
+    }
+}
---
+++ Bookstore/src/Bookstore.Service/Program.cs
@@ -0,0 +1,46 @@
+using Rhetos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(type => type.ToString()));
+
+builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
+        .ConfigureRhetosAppDefaults()
+        .UseBuilderLogProviderFromHost(serviceProvider)
+        .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
+    .AddAspNetCoreIdentityUser()
+    .AddHostLogging()
+    .AddDashboard()
+    .AddRestApi(o => 
+        {
+            o.BaseRoute = "rest";
+            o.GroupNameMapper = (conceptInfo, controller, oldname) => "v1";
+        });
+
+
+var app = builder.Build();
+
+app.UseRhetosRestApi();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+    app.MapRhetosDashboard();
+
+app.Run();
