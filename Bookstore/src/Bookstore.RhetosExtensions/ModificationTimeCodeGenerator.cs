c0a645f1c9404392b3378c8523a930bc30c05f38
zsokol-omega <zsokol@omega-software.hr>
2025-04-08 16:11:16 +0200

    Day4: ModificationTimeCodeGenerator valid code only for updated items

--- Bookstore/src/Bookstore.RhetosExtensions/ModificationTimeCodeGenerator.cs
+++ Bookstore/src/Bookstore.RhetosExtensions/ModificationTimeCodeGenerator.cs
@@ -17,9 +17,8 @@ namespace Bookstore.RhetosExtension
             $@"{{ 
                 var now = _executionContext.SqlUtility.GetDatabaseTime(_executionContext.SqlExecuter);
 
-                foreach (var newItem in insertedNew)
-                    if(newItem.{info.Property.Name} == null)
-                        newItem.{info.Property.Name} = now;
+                foreach (var updatedItem in updatedNew)
+                        updatedItem.{info.Property.Name} = now;
             }}
             ";
 
