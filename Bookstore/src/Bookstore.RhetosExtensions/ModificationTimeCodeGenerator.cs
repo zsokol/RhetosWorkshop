5fbfdfbad6173b8189ed5ced0888f18269e9b8fe
zsokol-omega <zsokol@omega-software.hr>
2025-04-08 14:02:49 +0200

    Day4: code table macro, attempt for code generator

---
+++ Bookstore/src/Bookstore.RhetosExtensions/ModificationTimeCodeGenerator.cs
@@ -0,0 +1,29 @@
+﻿//using System.ComponentModel.Composition;
+//using Rhetos.Compiler;
+//using Rhetos.Dom.DefaultConcepts;
+//using Rhetos.Extensibility;
+
+//namespace Bookstore.RhetosExtension
+//{
+//    [Export(typeof(IConceptCodeGenerator))]
+//    [ExportMetadata(MefProvider.Implements, typeof(ModificationTimeInfo))]
+//    public class ModificationTimeCodeGenerator : IConceptCodeGenerator
+//    {
+//        public void GenerateCode(Rhetos.Dsl.IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+//        {
+//            var info = (ModificationTimeInfo)conceptInfo;
+
+//            string snippet =
+//            $@"{{ 
+//                var now = _executionContext.SqlUtility.GetDatabaseTime(_executionContext.SqlExecuter);
+
+//                foreach (var newItem in insertedNew)
+//                    if(newItem.{info.Property.Name} == null)
+//                        newItem.{info.Property.Name} = now;
+//            }}
+//            ";
+
+//            codeBuilder.InsertCode(snippet, WritableOrmDataStructureCodeGenerator.InitializationTag, info.Property.DataStructure);
+//        }
+//    }
+//}
---
+++ Bookstore/src/Bookstore.RhetosExtensions/ModificationTimeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+
+namespace Bookstore.RhetosExtension
+{
+    // <summary>
+    /// Automatically enters time when the records was created.
+    /// </summary>
+    //[Export(typeof(IConceptInfo))]
+    [ConceptKeyword("ModificationTime")]
+    public class ModificationTimeInfo
+    {
+        [ConceptKey]
+        public DateTimePropertyInfo Property { get; set; }
+    }
+}
