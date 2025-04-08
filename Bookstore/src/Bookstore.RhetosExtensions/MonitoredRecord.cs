f3def650e5e0cca45e565c17b449ced7870de0b1
zsokol-omega <zsokol@omega-software.hr>
2025-04-08 16:26:18 +0200

    Day4: assignment MonitoredRecord part 1

---
+++ Bookstore/src/Bookstore.RhetosExtensions/MonitoredRecord.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+
+namespace Bookstore.RhetosExtensions
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("MonitoredRecord")]
+    public class MonitoredRecordInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public EntityInfo Entity { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class MonitoredRecordMacro : IConceptMacro<MonitoredRecordInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(MonitoredRecordInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            var logging = new EntityLoggingInfo
+            {
+                Entity = conceptInfo.Entity
+            };
+            newConcepts.Add(logging);
+
+            var createdAt = new DateTimePropertyInfo 
+            { 
+                DataStructure = conceptInfo.Entity,
+                Name = "CreatedAt"
+            };
+            newConcepts.Add(createdAt);
+
+            return newConcepts;
+        }
+    }
+}
